982bc4c299bf6f5b92060f11e2766ba58fa093d5
ololoshka2871 <SweetTreasure@2ch.hk>
2022-05-17 10:44:30 +0300

    some reformat code

--- src/main.rs
+++ src/main.rs
@@ -42,65 +42,6 @@ struct Cli {
     verbose: bool,
 }
 
-fn get_dir(p: &Option<PathBuf>, direction: &str, current_dir: &PathBuf) -> PathBuf {
-    if let Some(src) = p {
-        println!("{} dirrectory: {:?}", direction, src);
-        src.clone()
-    } else {
-        println!(
-            "{} dirrctory not specified, use current: {:?}",
-            direction, current_dir
-        );
-        current_dir.clone()
-    }
-}
-
-fn verify_input(src_dir: &PathBuf, dest_dir: &PathBuf, full: bool) -> Result<(), String> {
-    if !src_dir.is_dir() {
-        return Err(format!(
-            "Dirrectory {:?} is not exists or not a dirrectory",
-            src_dir
-        ));
-    }
-    let data_file_path = src_dir.join(if full {
-        FULL_DUMP_FILE_NAME
-    } else {
-        USED_DUMP_FILE_NAME
-    });
-    if !data_file_path.exists() {
-        return Err(format!("Data file {:?} not found!", data_file_path));
-    }
-    let config_file_path = src_dir.join(CONFIG_FILE_NAME);
-    if !config_file_path.exists() {
-        return Err(format!(
-            "Configuration file {:?} not found!",
-            config_file_path
-        ));
-    }
-    let storage_cfg_file_path = src_dir.join(STORAGE_FILE_NAME);
-    if !storage_cfg_file_path.exists() {
-        return Err(format!(
-            "Storage configuration file {:?} not found!",
-            config_file_path
-        ));
-    }
-    if !dest_dir.is_dir() {
-        return Err(format!(
-            "Destination dirrectory {:?} is not found!",
-            dest_dir
-        ));
-    }
-    Ok(())
-}
-fn chain_folder(chain: usize) -> String {
-    format!("chain-{}", chain)
-}
-
-fn create_chain_filder(dest: &PathBuf, chain: usize) {
-    std::fs::create_dir(dest.join(chain_folder(chain)))
-        .expect(format!("Failed to create dirrectory {}", chain_folder(chain)).as_str())
-}
-
 fn main() {
     let args = Cli::from_args();
     let current_dir = env::current_dir().expect("Can't get current dirrectory");
@@ -122,8 +63,8 @@ fn main() {
     let storage_cfg: mem_info::MemInfo =
         serde_json::from_str(json_data.as_str()).expect("Failed to parse storage configuration");
 
-    println!("Reading data, it may take several minutes...");
     let unpacked_pages = {
+        println!("Reading data, it may take several minutes...");
         let file = std::fs::File::open(src.join(if args.full {
             FULL_DUMP_FILE_NAME
         } else {
@@ -144,23 +85,13 @@ fn main() {
         )
     };
 
-    let chain_starts = unpacked_pages
-        .par_iter()
-        .enumerate()
-        .filter_map(|(i, page)| {
-            if page.header.prev_block_id == 0 && page.header.this_block_id == 0 {
-                Some(i)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<usize>>();
-
-    if args.verbose {
-        println!("Detected data chains:");
-    }
     let chains = {
+        if args.verbose {
+            println!("Detected data chains:");
+        }
         let mut chains = Vec::<&[PageData]>::new();
+        let chain_starts = detect_chains_starts(&unpacked_pages);
+
         // first -> last
         (0..chain_starts.len() - 1)
             .enumerate()
@@ -244,4 +175,77 @@ fn main() {
                 }
             })
     });
+}
+
+fn detect_chains_starts(unpacked_pages: &Vec<PageData>) -> Vec<usize> {
+    unpacked_pages
+        .par_iter()
+        .enumerate()
+        .filter_map(|(i, page)| {
+            if page.header.prev_block_id == 0 && page.header.this_block_id == 0 {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<usize>>()
+}
+
+fn get_dir(p: &Option<PathBuf>, direction: &str, current_dir: &PathBuf) -> PathBuf {
+    if let Some(src) = p {
+        println!("{} dirrectory: {:?}", direction, src);
+        src.clone()
+    } else {
+        println!(
+            "{} dirrctory not specified, use current: {:?}",
+            direction, current_dir
+        );
+        current_dir.clone()
+    }
+}
+
+fn verify_input(src_dir: &PathBuf, dest_dir: &PathBuf, full: bool) -> Result<(), String> {
+    if !src_dir.is_dir() {
+        return Err(format!(
+            "Dirrectory {:?} is not exists or not a dirrectory",
+            src_dir
+        ));
+    }
+    let data_file_path = src_dir.join(if full {
+        FULL_DUMP_FILE_NAME
+    } else {
+        USED_DUMP_FILE_NAME
+    });
+    if !data_file_path.exists() {
+        return Err(format!("Data file {:?} not found!", data_file_path));
+    }
+    let config_file_path = src_dir.join(CONFIG_FILE_NAME);
+    if !config_file_path.exists() {
+        return Err(format!(
+            "Configuration file {:?} not found!",
+            config_file_path
+        ));
+    }
+    let storage_cfg_file_path = src_dir.join(STORAGE_FILE_NAME);
+    if !storage_cfg_file_path.exists() {
+        return Err(format!(
+            "Storage configuration file {:?} not found!",
+            config_file_path
+        ));
+    }
+    if !dest_dir.is_dir() {
+        return Err(format!(
+            "Destination dirrectory {:?} is not found!",
+            dest_dir
+        ));
+    }
+    Ok(())
+}
+fn chain_folder(chain: usize) -> String {
+    format!("chain-{}", chain)
+}
+
+fn create_chain_filder(dest: &PathBuf, chain: usize) {
+    std::fs::create_dir(dest.join(chain_folder(chain)))
+        .expect(format!("Failed to create dirrectory {}", chain_folder(chain)).as_str())
 }
